3d77239c5f30e5a272fde0ba037e9b7d3ec4d580
zonatime <zonatime@users.noreply.github.com>
2015-06-07 16:20:41 -0700

    Update eda_assignment_1_plot_4.R
    Ready for submission

--- eda_assignment_1_plot_4.R
+++ eda_assignment_1_plot_4.R
@@ -1,36 +1,17 @@
+# eda_assignment_1_plot4.R
 # Plot graph #4
 #
-# set up data frame to receive subset of full data file
-epc_data = data.frame(Date = character(), 
-                      Time = character(), 
-                      Global_active_power = numeric(), 
-                      Global_reactive_power = numeric(),
-                      Voltage = numeric(),
-                      Global_intensity = numeric(),
-                      Sub_metering_1 = numeric(),
-                      Sub_metering_2 = numeric(),
-                      Sub_metering_3 = numeric(),
-                      stringsAsFactors = FALSE)
+# source the data handling functions
+source("get_EPC_data.R")
 
-# open connection to raw data file (very large)
-conn <- file("household_power_consumption.txt", "r")
+# check for data, download and unzip if not present
+source_file = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+dest_file = "exdata-data-household_power_consumption.zip"
+data_file = "household_power_consumption.txt"
+check_EPC_data(source_file, dest_file, data_file)
 
-# set counter for while loop
-# read 1 line at at time to test
-# if (2|3)/02/2007 write line to data frame 
-i=0
-while(length(line <- readLines(conn, 1)) > 0) {
-        ind <- grep("^(1|2)\\/2\\/2007", line)
-        if (length(ind)) {
-                vector = unlist(strsplit(line, ";"))
-                i=i+1
-                for (j in 1:2) epc_data[i,j] = as.character(vector[j])
-                for (j in 3:9) epc_data[i,j] = as.numeric(vector[j])
-        }
-}
-
-# close connection'
-close(conn)
+# subset for "1/2/2007" and "2/2/2007"
+epc_data <- subset_EPC_data(data_file)
 
 # combine Date & Time; convert to DateTime value
 epc_data$DateTime <- paste(epc_data$Date, epc_data$Time)
@@ -49,24 +30,23 @@ par(mfrow=c(2,2))
 xrange <- range(epc_data$DateTime) 
 yrange <- range(epc_data$Global_active_power)
 
-#
+# set up margins for all plots
+par (mar = c(5, 4, 1, 1), mgp=c(3, 1, 0))
+
 # set up  plot #1 
-par (mar = c(3, 4, 1, 1))
 plot(xrange, yrange, type="n", xlab ="",
-     ylab="Global Active Power (kilowatts)") 
+     ylab="Global Active Power") 
 
 # plot the lines for #1
 lines(epc_data$DateTime, epc_data$Global_active_power, lty=1) 
 
 # plot #2 -  line chart for Voltage
-
 # get the range for the x and y axis for plot #2
 xrange <- range(epc_data$DateTime) 
 yrange <- range(epc_data$Voltage)
 #
 # set up  plot #2 
-par (mar = c(3, 4, 1, 1))
-plot(xrange, yrange, type="n", xlab ="",
+plot(xrange, yrange, type="n", xlab ="datetime",
      ylab="Voltage") 
 
 # plot the lines for #2
@@ -83,7 +63,6 @@ ymax <- max(c(max(epc_data$Sub_metering_1),
 yrange <- c(0,ymax)
 
 # set up  plot #3 
-par (mar = c(3, 4, 1, 1))
 plot(xrange, yrange, type="n", xlab ="",
      ylab="Energy sub metering") 
 
@@ -93,7 +72,7 @@ lines(epc_data$DateTime, epc_data$Sub_metering_2, lty=1, col = "red")
 lines(epc_data$DateTime, epc_data$Sub_metering_3, lty=1, col = "blue")
 legend("topright", col = c("black", "red", "blue"), 
        c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lty=c(1,1,1))
+       lty=c(1,1,1), bty="n")
 
 #
 # plot #4 -  line chart for Global_reactive_power
@@ -103,8 +82,7 @@ xrange <- range(epc_data$DateTime)
 yrange <- range(epc_data$Global_reactive_power)
 #
 # set up  plot #4 
-par (mar = c(3, 4, 1, 1))
-plot(xrange, yrange, type="n", xlab ="",
+plot(xrange, yrange, type="n", xlab ="datetime",
      ylab="Global_reactive_power") 
 
 # plot the lines for #4
